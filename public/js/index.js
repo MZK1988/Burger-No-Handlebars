d9d797b0587aa0346fe4ffad90a49a5b6c5683db
MZK1988 <max@korenvaes.com>
2019-01-06 12:47:14 -0600

    another commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,47 @@
 $(document).ready(function () {
+
+    //This grabs all of the orders in the database as soon as the page loads
+    //Also even if the user presses refresh
     $.get("/api/orders", function (data) {
+        //Loops through the "data"/all of the orders in the database after the "get"
         for (i = 0; i < data.length; i++) {
             var id = data[i].id
             var body = data[i].body
             
             var devour = data[i].devour
 
+            //if the order has not been devoured, it runs the appendTable function
             if (!devour) {
                 appendTable(id, body, devour);
             } else { console.log("") }
         }
     })
 
+    //This variable is global, but I think would work just fine as local variable to teh submit on click
     var orderInput = $("#exampleFormControlTextarea1");
 
     $("#submitOne").on("click", function () {
         event.preventDefault();
+        //This creates the newOrder variable to be submitted as "data" into the submitOrder function
         var newOrder = {
             body: orderInput.val().trim()
         }
-
-
         submitOrder(newOrder);
-
-
-
+        
+        
     });
 
     //interesting that if you generate dynamic buttons, then you have to use below syntax as a kind of distributed 
     $(document).on('click', '.devour', function () {
+        
         var unparsedvalue = $(this).attr("data-value");
 
         //need to parse this integer in order to match up with an incoming data element from the get that comes later
         var value = parseInt(unparsedvalue);
 
         $("#rowId" + value).hide();
+
+        
 
         //have to grab the body for the body of the text...do get from DOM or do get by reaching into the data model again 
 
@@ -49,21 +56,23 @@ $(document).ready(function () {
                         body: bodyId,
                         devour: true
                     }
-
                     updateOrder(updatedOrder);
                     // removeDevoured();
                     //remove table data if devour property value = true
-
+                    
                 } else { console.log("we have an error") }
             }
-
+            //this is an empty promise, but likely can remove from the get
         }).then(function () {
-
+            
+            
         })
-
+        window.location = "/order/" + unparsedvalue
     });
 
-    //STORING FUNCTIONS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    
+    //STORING FUNCTIONS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++   
+    
+    //SUBMIT ORDER FUNCTION===============================================================================================
     function submitOrder(Order) {
         console.log(Order)
         $.post("/api/new", Order).then(function () {
@@ -81,11 +90,12 @@ $(document).ready(function () {
 
     }
 
-
+    //APPEND TABLE FUNCTION===============================================================================================
     function appendTable(id, body, devour) {
-        $("#tableBody").append("<tr id=rowId" + id + " data-value = " + id + "><th>" + id + "</th><td id=bodyId" + id + ">" + body + "</td><td><button class = devour data-value = " + id + ">Devour!</button></td></tr>")
+        $("#tableBody").append("<tr id=rowId" + id + " data-value = " + id + "><th>" + id + "</th><td id=bodyId" + id + ">" + body + "</td><td><button href=/order class = devour data-value = " + id + ">Devour!</button></td></tr>")
     }
 
+    //UPDATE ORDER FUNCTION===============================================================================================
     //can create any variable and pass where order is being passed in the updateOrder function...
     function updateOrder(Order) {
         $.ajax({
--- routes/order-html-routes.js
+++ routes/order-html-routes.js
@@ -3,25 +3,33 @@ var db = require("../models")
 var path = require("path")
 
 module.exports = function (app) {
-
     app.get("/order", function (req, res) {
-
-        db.Order.findAll({}).then(function(dbOrder) {
-
+        db.Order.findAll({}).then(function (dbOrder) {
             //can likely filter out the devoured orders here
-            
             var dbStuff = {
                 dbOrder,
                 msg: "This is the Order!"
             }
-        
+            console.log(dbStuff);
             res.render("index", dbStuff)
         })
         // Handlebars requires an object to be sent to the dog handlebars file.
-        
-
-
-
     })
+
+
+    app.get("/order/:id", function (req, res) {
+        db.Order.findAll({}).then(function (dbOrder) {
+            var dbStuff = {
+                dbOrder,
+                msg: "this is a burger"
+            }
+
+            console.log(dbStuff);
+            res.render("order", dbStuff
+            );
+        });
+    });
+
+
 
 }
